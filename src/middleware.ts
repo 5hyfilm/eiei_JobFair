2085131938ba5f1b01dd5ec327159c9d1e5da40c
フィルム <waranthorn_c@outlook.com>
2024-11-19 14:43:34 +0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,5 @@
 export {default} from 'next-auth/middleware'
-// export default withAuth({
-//     pages: {
-//         signIn: '/signIn',
-//     },
-// });
 
-export const config = {
-    matcher: ["/mainpage", "/myBooking", "/manage", "/myProfile"],
-};
+export const config ={
+    matcher: ["/mainpage","/myBooking","/manage","/myProfile"]
+}
